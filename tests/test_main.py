1ff311a0b678288bd59f7be8447e396bf8914e69
jordantw2014 <jordantw2014@gmail.com>
2023-05-01 15:12:40 -0700

    Changed tests to apply altered code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,21 @@
 """
 Test the main module.
-Author: Wolf Paulus (wolf@paulus.com)
+Author: Jordan Wallschlaeger
 """
 from unittest import TestCase
-from main import is_odd, is_odd_str
+from main import is_div7, is_div7_str
 
 
 class Test(TestCase):
     def test_is_odd(self):
-        assert not is_odd(0)
-        assert is_odd(1)
-        assert not is_odd(2)
+        assert is_div7(0)
+        assert is_div7(1)
+        assert not is_div7(7)
 
     def test_is_odd_str(self):
-        assert is_odd_str("0") == "0 is even."
-        assert is_odd_str("1") == "1 is odd."
-        assert is_odd_str("2") == "2 is even."
-        assert is_odd_str("-1") == "Please enter a number."
-        assert is_odd_str("A") == "Please enter a number."
-        assert is_odd_str("") == "Please enter a number."
+        assert is_div7_str("0") == "0 is not divisible by 7."
+        assert is_div7_str("1") == "1 is not divisible by 7."
+        assert is_div7_str("7") == "7 is divisible by 7."
+        assert is_div7_str("-1") == "Please enter a number."
+        assert is_div7_str("A") == "Please enter a number."
+        assert is_div7_str("") == "Please enter a number."
