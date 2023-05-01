89c30be8ad713f91d56854f22e1f270d44dcc50d
jordantw2014 <jordantw2014@gmail.com>
2023-05-01 15:13:26 -0700

    Changed to not be odd/even checker

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 """
 odd/even number checker
-Author: Wolf Paulus (https://wolfpaulus.com)
+Author: Jordan Wallschlaeger
 """
 
 
-def is_odd(num: int) -> bool:
-    """Return True if num is odd, False otherwise."""
-    return num % 2 == 1
+def is_div7(num: int) -> bool:
+    """Return True if num is not divisible by 7, False otherwise."""
+    if num == 0:
+        return True
+    return num % 7 != 0
 
 
-def is_odd_str(num: str) -> str:
-    """Return a string indicating whether num is odd or even."""
+def is_div7_str(num: str) -> str:
+    """Return a string indicating whether num is divisible by 7 or not."""
     if num.isnumeric():
-        return f"{num} is {'odd' if is_odd(int(num)) else 'even'}."
+        return f"{num} is {'not divisible by 7' if is_div7(int(num)) else 'divisible by 7'}."
     else:
         return "Please enter a number."
